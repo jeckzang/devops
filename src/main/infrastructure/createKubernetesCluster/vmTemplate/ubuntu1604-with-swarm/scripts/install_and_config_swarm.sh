5a10ae00a462851ed6c024b1aaacf817790e409c
Jeck Zhang <jeck.zhang@polycom.com>
2019-06-11 13:07:00 +0800

    finish ubuntu install

--- src/main/infrastructure/createKubernetesCluster/vmTemplate/ubuntu1604-with-swarm/scripts/install_and_config_swarm.sh
+++ src/main/infrastructure/createKubernetesCluster/vmTemplate/ubuntu1604-with-swarm/scripts/install_and_config_swarm.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-echo devops | sudo -S apt-get update
+apt-get update
+
+apt-get install -y apt-transport-https ca-certificates gnupg-agent software-properties-common 
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-echo devops | sudo -S add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-echo devops | sudo -S apt-get update
+apt-get update
 
-echo devops | sudo -S apt-get install docker-ce docker-ce-cli containerd.io
+apt-get -y install docker-ce docker-ce-cli containerd.io
+
+# Add `sync` so Packer doesn't quit too early, before the large file is deleted.
+sync

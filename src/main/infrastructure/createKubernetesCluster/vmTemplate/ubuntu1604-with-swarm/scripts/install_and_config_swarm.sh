fa6ea6780d023ecb0b4a54ddde409e29c5edfb9a
Jeck Zhang <jeck.zhang@polycom.com>
2019-06-11 10:11:09 +0800

    change install shell

--- src/main/infrastructure/createKubernetesCluster/vmTemplate/ubuntu1604-with-swarm/scripts/install_and_config_swarm.sh
+++ src/main/infrastructure/createKubernetesCluster/vmTemplate/ubuntu1604-with-swarm/scripts/install_and_config_swarm.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo 'devops' | sudo -S apt-get update
+echo devops | sudo -S apt-get update
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+echo devops | sudo -S add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+echo devops | sudo -S apt-get update
+
+echo devops | sudo -S apt-get install docker-ce docker-ce-cli containerd.io

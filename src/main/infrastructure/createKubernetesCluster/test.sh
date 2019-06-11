7675aa6353d733096861ed49c4cee18a36e378a9
Jeck Zhang <jeck.zhang@polycom.com>
2019-06-11 16:50:04 +0800

    commit for ubuntu swarm change ip

--- src/main/infrastructure/createKubernetesCluster/test.sh
+++ src/main/infrastructure/createKubernetesCluster/test.sh
@@ -6,6 +6,7 @@ packer build -var-file=vcenter.json centos7.json
 
 # use ansible to download VM disk files to local
 ansible-playbook -e "ansible_ssh_pass=Polycom123" vsphere.yml --check
+ansible-playbook -e "ansible_become_pass=devops" swarm-master.yml --check
 
 # 
 terraform plan -var-file=envParams/vmm.tfvars && terraform apply -var-file=envParams/vmm.tfvars
